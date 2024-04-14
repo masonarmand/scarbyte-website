c08bc91dfed1769cbf764436fad4d0e5c0217345
masonarmand <masonarmand1@gmail.com>
2024-04-14 15:43:15 -0400

    .

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
-git add .
-git commit -m "."
-git push
+#!/bin/bash
 txt2web /var/www/html
 python3 img2gal.py /var/www/html gallery/template.html
 rm /var/www/html/gallery/template.html
-neocities push /var/www/html
+
+if [[ "$1" == "--push" ]]; then
+    git add .
+    git commit -m "."
+    git push
+    neocities push /var/www/html
+fi
--- build_no_push.sh
+++
@@ -1,3 +0,0 @@
-txt2web /var/www/html
-python3 img2gal.py /var/www/html gallery/template.html
-rm /var/www/html/gallery/template.html
